e648c641968607cbb992bbe05a01526686d0d91a
Jake Steven Bland <bn826011@bdan16.local>
2018-11-13 11:11:58 +0000

    added new, sillier function

--- do_math.py
+++ do_math.py
@@ -3,3 +3,11 @@ import numpy as np
 def mathy(numbers):
 
     return sum(numbers)
+
+def mathier(numbers, more_numbers):
+
+    difference = numbers - more_numbers
+
+    frac_diff = difference/numbers
+
+    return frac_diff
